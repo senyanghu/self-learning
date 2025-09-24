4455a179e942c6f77d33dc280b8d6056b2481c4d
Senyang Hu <senyanghu@gmail.com>
2025-09-23 22:33:08 -0700

    Rename nums into num for moveZero java solution

--- Lai_2024/A_01_Array/MoveZero.java
+++ Lai_2024/A_01_Array/MoveZero.java
@@ -1,27 +1,27 @@
 package A_01_Array;
 
 public class MoveZero {
-    public void moveZeroToRight(int[] nums) {
+    public void moveZeroToRight(int[] num) {
         int insertPosition = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                swap(nums, insertPosition++, i);
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] != 0) {
+                swap(num, insertPosition++, i);
             }
         }
     }
 
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    private void swap(int[] num, int i, int j) {
+        int temp = num[i];
+        num[i] = num[j];
+        num[j] = temp;
     }
 
     public static void main(String[] args) {
         MoveZero moveZero = new MoveZero();
-        int[] nums = {0, 1, 0, 3, 12};
-        moveZero.moveZeroToRight(nums);
-        for (int i = 0; i < nums.length; i++) {
-            System.out.print(nums[i] + " ");
+        int[] num = {0, 1, 0, 3, 12};
+        moveZero.moveZeroToRight(num);
+        for (int i = 0; i < num.length; i++) {
+            System.out.print(num[i] + " ");
         }
     }
 }
