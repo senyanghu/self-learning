350d58820670175a7f7fb09818b6503add522349
Senyang Hu <senyanghu@gmail.com>
2025-10-12 09:59:13 -0700

    Add a comment about big O for isBST

--- Lai_2024/A_04_Tree/Q07_IsBST.java
+++ Lai_2024/A_04_Tree/Q07_IsBST.java
@@ -2,6 +2,14 @@ package A_04_Tree;
 
 import Utils.TreeNode;
 
+/*时间复杂度：O(n)
+** 每个节点只访问一次
+** 每个节点的操作是 O(1)
+** 总时间：n × O(1) = O(n)
+
+空间复杂度：O(h)
+** h 是树的高度
+** 递归栈的深度 */
 public class Q07_IsBST {
     public boolean isValidBST(TreeNode root) {
         return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
